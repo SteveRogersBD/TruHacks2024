6ec5e9b64cb2157e142abe89cf63faed3fc2e1fb
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-03-23 08:22:24 -0500

    History, Note added

---
+++ app/src/main/java/com/example/truhacks/Adapters/SearchAdapter.java
@@ -0,0 +1,2 @@
+package com.example.truhacks.Adapters;public class SearchAdapter {
+}
---
+++ app/src/main/java/com/example/truhacks/PeriodActivity.java
@@ -0,0 +1,14 @@
+package com.example.truhacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeriodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_period);
+    }
+}
