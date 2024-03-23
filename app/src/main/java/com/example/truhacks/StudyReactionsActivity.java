39648b2c87fc27a4730485685bcc974b55fc777f
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-03-23 14:18:25 -0500

    DataBase Complete

---
+++ app/src/main/java/com/example/truhacks/StudyReactionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.truhacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyReactionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_reactions);
+    }
+}
